d270628cce4e8dbac775f64c3f8c85b45ffe987d
kaadon.com <kaadon.com@gmail.com>
2024-04-27 18:29:00 +0800

    修正

--- src/balance/index.js
+++ src/balance/index.js
@@ -2,7 +2,7 @@ export const getBalance = async (web3Client, address, contract = null) => {
     try {
         //逻辑代码
         if (contract == null) {
-            return Promise.resolve(await web3Client.eth.getBalance(address));
+            return Promise.resolve(web3Client.client.utils.fromWei(await web3Client.eth.getBalance(address),'ether'));
         } else {
             return Promise.resolve(web3Client.client.utils.fromWei(await (new web3Client.client.eth.Contract(JSON.parse(contract.ABI), contract.address)).methods.balanceOf(address).call(), contract.EtherUnits));
         }
